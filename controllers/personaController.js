16202dc27b25be769a06566287a6bfb862cff59d
Martuking <martinsch1@gmail.com>
2018-03-11 20:36:19 -0300

    agregue validadores para persona

--- controllers/personaController.js
+++ controllers/personaController.js
@@ -37,20 +37,24 @@ exports.persona_create_get = function(req, res, next) {
 };
 
 // Handle persona create on POST
-exports.persona_create_post = function(req, res) {
-    /*body('cod').isLength({ min: 1 }).trim().withMessage('El código tiene que ser especificado'),
-    body('apellido1').isLength({ min: 1 }).trim().withMessage('El primer apellido tiene que ser especificado')
-        .isAlphanumeric().withMessage('El primer apellido no puede contener numeros'),
-    body('apellido2').isLength({ min: 1 }).trim().withMessage('El segundo apellido tiene que ser especificado')
-        .isAlphanumeric().withMessage('El segundo apellido no puede contener numeros'),
-    body('nombre').isLength({ min: 1 }).trim().withMessage('El nombre tiene que ser especificado')
-        .isAlphanumeric().withMessage('El nombre no puede contener numeros'),
-    body('rut').isLength({ min: 1 }).trim().withMessage('El rut tiene que ser especificado'),
-    body('domicilio').isLength({ min: 1 }).trim().withMessage('El domicilio tiene que ser especificado'),
-    body('cargo').isLength({ min: 1 }).trim().withMessage('El cargo tiene que ser especificado')
-        .isAlphanumeric().withMessage('El cargo no puede contener numeros'),*/
-
-};
+/*exports.persona_create_post = function(req, res) {
+    body('cod').isLength({ min: 1 }).trim().isAlphanumeric,
+    body('apellido1').isLength({ min: 1 }).trim().isAlphanumeric(),
+    body('apellido2').isLength({ min: 1 }).trim().withMessage.isAlphanumeric().,
+    body('nombre').isLength({ min: 1 }).trim().isAlphanumeric(),
+    body('rut').isLength({ min: 1 }).trim().isAlphanumeric,
+    body('domicilio').isLength({ min: 1 }).trim().isAlphanumeric.,
+    body('cargo').isLength({ min: 1 }).trim().isAlphanumeric(),
+    body('dp').isLength({ min: 1 }).trim().isAlphanumeric().withMessage('DP es un numero'),
+    body('cargo').isLength({ min: 1 }).trim().isAlphanumeric(),
+    body('sexo').isLength({ min: 1 }).trim().isAlphanumeric(),
+    body('fechaNacimiento').optional({ checkFalsy: true }).isISO8601(),
+    body('fechaIngreso').optional({ checkFalsy: true }).isISO8601(),
+    body('sueldoBase').isLength({ min: 1 }).trim().isAlphanumeric(),
+    body('estadoCivil').isLength({ min: 1 }).trim().isAlphanumeric(),
+    body('porcentaCom').isLength({ min: 1 }).trim().isAlphanumeric(),
+    body('afp').isLength({ min: 1 }).trim().isAlphanumeric(),
+};*/
 
 // Display persona delete form on GET
 exports.persona_delete_get = function(req, res) {
